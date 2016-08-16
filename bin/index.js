ae37f21fa7afd1e22a5fa67640fbd4561cf2e7f6
Rui Quelhas <rfpquelhas@gmail.com>
2016-08-16 17:00:09 +0100

    Add support for TLS connections

--- bin/index.js
+++ bin/index.js
@@ -2,22 +2,43 @@
 
 'use strict';
 
+const Fs = require('fs');
 const Hapi = require('hapi');
+const Package = require('../package.json');
+const Program = require('commander');
 const Resolver = require('../');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+
+Program
+  .version(Package.version)
+  .option('-k, --key [key]', 'TLS key')
+  .option('-c, --cert [cert]', 'TLS certificate')
+  .parse(process.argv);
+
+if (!Program.key || !Program.cert) {
+    server.connection({ port: 3000 });
+}
+else {
+    server.connection({
+        port: 3000,
+        tls: {
+            key: Fs.readFileSync(Program.key),
+            cert: Fs.readFileSync(Program.cert)
+        }
+    });
+}
 
 server.register(Resolver, (err) => {
 
     if (err) {
-        throw err;
+        return callback(err);
     }
 
     server.start((err) => {
 
         if (err) {
-            throw err;
+            return callback(err);
         }
 
         console.log(`Server running at: ${server.info.uri}`);
